1e4bf200c69cc1aa3ba0ac63890d863fb8ff686d
abhinav1829 <abhinavdhoka@gmail.com>
2020-07-15 01:02:56 +0530

    CSS added
    Bootstrap inculcated

--- Borrower/forms.py
+++ Borrower/forms.py
@@ -7,3 +7,8 @@ class RegisterBorrowerForm(forms.ModelForm):
     class Meta:
         model = FairTradeBorrower
         exclude = ['account_type', 'repayment_score']
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterBorrowerForm, self).__init__(*args, **kwargs)
+        for value in self.fields.values():
+            value.widget.attrs.update({'class' : 'form-control'})
--- Lender/forms.py
+++ Lender/forms.py
@@ -7,3 +7,8 @@ class RegisterLenderForm(forms.ModelForm):
     class Meta:
         model = FairTradeLender
         exclude = ['account_type']
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterLenderForm, self).__init__(*args, **kwargs)
+        for value in self.fields.values():
+            value.widget.attrs.update({'class' : 'form-control'})
